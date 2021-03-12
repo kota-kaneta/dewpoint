11d9e00028665107693567c66b4fd89b317ef960
kota-kaneta <foruri@icloud.com>
2021-03-13 07:34:06 +0900

    first

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,43 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app class="app">
+    <div class="top-nav">
+      <div class="nav" v-scroll-to="'#home'">Home</div>
+      <div class="nav" v-scroll-to="'#about'">About</div>
+      <div class="nav" v-scroll-to="'#services'">Services</div>
+      <div class="nav" v-scroll-to="'#works'">Works</div>
+      <div class="nav" v-scroll-to="'#contact'">Contact</div>
+    </div>
+  </v-app>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+<style lang="scss">
+.app{
+  position: relative;
+  box-sizing: border-box;
+  width: 100%;
+  overflow: auto;
+  overflow-x: hidden;
+  font-size: 1.4rem;
+  line-height: 1;
+  font-family: "Noto Sans JP", sans-serif;
+  color: #000000;
+  background-color: #ffffff;
 }
-</script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.top-nav{
+  position: fixed;
+  top: 20px;
+  right: 50px;
+  display: flex;
+  font-size: 16px;
+}
+.nav{
+  cursor: pointer;
+  padding: 30px;
 }
 </style>
+
+<script>
+export default {
+  name: 'App'
+};
+</script>
